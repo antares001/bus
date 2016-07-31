2a5e1c6730a6b1e6510bda9c857eda5fae776552
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-07-31 08:48:10 +0300

    [REST]: add account, sum, newData

--- src/main/java/net/scnetwork/bus/rest/GetInfo.java
+++ src/main/java/net/scnetwork/bus/rest/GetInfo.java
@@ -3,27 +3,34 @@ package net.scnetwork.bus.rest;
 import net.scnetwork.bus.Dispatcher;
 import net.scnetwork.bus.domain.Response;
 import net.scnetwork.bus.domain.ResponseJs;
-import net.scnetwork.bus.enums.StatusEnum;
-import net.scnetwork.bus.utils.XmlUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GetInfo {
     private Dispatcher dispatcher = new Dispatcher();
 
-    @RequestMapping("/rest/get/echo")
+    @RequestMapping(value = "/rest/get/echo", method = RequestMethod.GET)
     public String echoGet(){
         return "test running";
     }
 
-    @RequestMapping("/rest/getXml")
-    public Response restGetXml(){
+    @RequestMapping("/rest/getXml/{service}")
+    public Response restGetXml(@PathVariable(value = "service") String service,
+                               @RequestParam(value = "account") String account){
+        switch (service){
+            default:
+                break;
+        }
         return dispatcher.restXmlDispatcher("");
     }
 
-    @RequestMapping("/rest/getJson")
-    public ResponseJs restGetJson(){
+    @RequestMapping(value = "/rest/getJson/{service}", method = RequestMethod.GET)
+    public ResponseJs restGetJson(@PathVariable(value = "service") String service,
+                                  @RequestParam(value = "account") String account){
+        switch (service){
+            default:
+                break;
+        }
         return dispatcher.restJsDispatcher("");
     }
 }
