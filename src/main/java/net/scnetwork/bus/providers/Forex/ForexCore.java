da87877c4db1aa10843dbfd2d4ad8769975db09c
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-07-31 09:21:37 +0300

    [ALL]: add IProviders

--- src/main/java/net/scnetwork/bus/providers/Forex/ForexCore.java
+++ src/main/java/net/scnetwork/bus/providers/Forex/ForexCore.java
@@ -4,8 +4,10 @@ import net.scnetwork.bus.domain.Data;
 import net.scnetwork.bus.domain.DataJs;
 import net.scnetwork.bus.domain.Response;
 import net.scnetwork.bus.domain.ResponseJs;
+import net.scnetwork.bus.providers.IProviders;
 
-public class ForexCore {
+public class ForexCore implements IProviders{
+    @Override
     public Response processingXml(Data data){
         Response result = new Response();
 
@@ -17,6 +19,7 @@ public class ForexCore {
         return result;
     }
 
+    @Override
     public ResponseJs processing(DataJs data){
         ResponseJs result = new ResponseJs();
 
