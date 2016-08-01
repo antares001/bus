2f1eb49abf79bbc085baeb6288dabc2a84fa5a95
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-08-01 22:49:10 +0300

    [FOREX]: add data& code response

--- src/main/java/net/scnetwork/bus/providers/Forex/ForexCore.java
+++ src/main/java/net/scnetwork/bus/providers/Forex/ForexCore.java
@@ -4,6 +4,10 @@ import net.scnetwork.bus.domain.Data;
 import net.scnetwork.bus.domain.DataJs;
 import net.scnetwork.bus.domain.Response;
 import net.scnetwork.bus.domain.ResponseJs;
+import net.scnetwork.bus.enums.StatusEnum;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ForexCore {
     public Response processingXml(Data data){
@@ -12,6 +16,8 @@ public class ForexCore {
         String account = data.getAccount();
 
         Data rData = new Data();
+        rData.setDate(new Date());
+        rData.setStatus(StatusEnum.OK);
         result.setData(rData);
 
         return result;
@@ -23,6 +29,8 @@ public class ForexCore {
         String account = data.getAccount();
 
         DataJs rData = new DataJs();
+        rData.setDate(new Date());
+        rData.setStatus(StatusEnum.OK);
         result.setData(rData);
 
         return result;
