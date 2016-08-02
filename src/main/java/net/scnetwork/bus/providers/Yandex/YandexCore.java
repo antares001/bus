f9e0951bbc2b00eb52f572c85f45ab2035c89e85
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-08-02 22:22:10 +0300

    [YANDEX]: add library

--- src/main/java/net/scnetwork/bus/providers/Yandex/YandexCore.java
+++ src/main/java/net/scnetwork/bus/providers/Yandex/YandexCore.java
@@ -1,14 +1,25 @@
 package net.scnetwork.bus.providers.Yandex;
 
+import com.yandex.money.api.YandexMoney;
 import net.scnetwork.bus.domain.Data;
 import net.scnetwork.bus.domain.DataJs;
 import net.scnetwork.bus.domain.Response;
 import net.scnetwork.bus.domain.ResponseJs;
 import net.scnetwork.bus.providers.IProviders;
 
+import java.security.Permission;
+import java.util.Collection;
+import java.util.LinkedList;
+
 public class YandexCore implements IProviders{
+    private static final String CLIENT_ID = "";
+
     @Override
     public Response processingXml(Data data) {
+        YandexMoney yandexMoney = new YandexMoney(CLIENT_ID);
+
+        Collection<Permission> scope = new LinkedList<>();
+
         return null;
     }
 
