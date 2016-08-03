bc9c3c8b328c6147eb2d0f6bea19cf3e8bb9b678
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-08-03 22:17:59 +0300

    [POS]: add mina listener.

---
+++ src/main/java/net/scnetwork/bus/MinaServer.java
@@ -0,0 +1,37 @@
+package net.scnetwork.bus;
+
+import net.scnetwork.bus.handlers.IsoHandler;
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
+@Component
+public class MinaServer {
+    private static final int PORT = 9123;
+
+    @PostConstruct
+    public void init(){
+        IoAcceptor acceptor = new NioSocketAcceptor();
+
+        acceptor.getFilterChain().addLast("logger", new LoggingFilter());
+        acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName("UTF-8"))));
+
+        acceptor.setHandler(new IsoHandler());
+        acceptor.getSessionConfig().setReadBufferSize(2048);
+        acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);
+        try {
+            acceptor.bind(new InetSocketAddress(PORT));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
