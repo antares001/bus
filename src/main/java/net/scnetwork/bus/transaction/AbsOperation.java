16efba183ef71b398319a859159a4ad361f6f243
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-08-03 22:47:59 +0300

    [TRANSACTIONAL]: add interface & implementation.

---
+++ src/main/java/net/scnetwork/bus/transaction/AbsOperation.java
@@ -0,0 +1,7 @@
+package net.scnetwork.bus.transaction;
+
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public class AbsOperation implements ITransactionOperation{
+}
---
+++ src/main/java/net/scnetwork/bus/transaction/ITransactionOperation.java
@@ -0,0 +1,5 @@
+package net.scnetwork.bus.transaction;
+
+public interface ITransactionOperation {
+    //TODO: add shared operation
+}
