7e6dbb9ad2a14b0970f8344dcdc89ff80d6e4c0b
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-08-05 23:14:07 +0300

    [YAML]: reformat.

--- src/main/java/net/scnetwork/bus/Bus.java
+++ src/main/java/net/scnetwork/bus/Bus.java
@@ -1,8 +1,6 @@
 package net.scnetwork.bus;
 
 import net.scnetwork.bus.service.XmlFunction;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,8 +9,6 @@ import javax.xml.ws.Endpoint;
 
 @SpringBootApplication
 public class Bus implements CommandLineRunner{
-    private static final Logger log = LoggerFactory.getLogger(Bus.class);
-
     public static void main(String... arg){
         SpringApplication.run(Bus.class, arg);
     }
