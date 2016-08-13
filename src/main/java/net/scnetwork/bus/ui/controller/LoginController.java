6f93a06345db5339375769b53f96099491ad80b3
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-08-13 10:13:50 +0300

    [UI]: remove webui.

--- src/main/java/net/scnetwork/bus/ui/controller/LoginController.java
+++ src/main/java/net/scnetwork/bus/ui/controller/LoginController.java
@@ -4,7 +4,8 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
 
 @SpringUI(path = "/admin/login")
@@ -15,6 +16,6 @@ public class LoginController extends UI{
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        setContent(new Label(PAGE));
+        setContent(new Button("Click", e -> Notification.show("hello")));
     }
 }
