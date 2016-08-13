93874ea608d49da21886fb351703474bcfad1065
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-08-13 10:11:30 +0300

    [UI]: add libs & security auth.

---
+++ src/main/java/net/scnetwork/bus/ui/controller/LoginController.java
@@ -0,0 +1,20 @@
+package net.scnetwork.bus.ui.controller;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+
+@SpringUI(path = "/admin/login")
+@Title("Авторизация")
+@Theme("valo")
+public class LoginController extends UI{
+    public static final String PAGE = "login";
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        setContent(new Label(PAGE));
+    }
+}
