4e1f2b39b70c77fedc4371b3bd14184709a52493
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-08-14 10:52:12 +0300

    [ISO8583]: add some impl.

---
+++ src/main/java/net/scnetwork/bus/config/modules/Jpos.java
@@ -0,0 +1,44 @@
+package net.scnetwork.bus.config.modules;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "idc")
+public class Jpos implements IModules{
+    private boolean use;
+    private String service;
+    private String url;
+
+    @Override
+    public boolean isUse() {
+        return use;
+    }
+
+    @Override
+    public void setUse(boolean use) {
+        this.use = use;
+    }
+
+    @Override
+    public String getService() {
+        return service;
+    }
+
+    @Override
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    @Override
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ src/main/java/net/scnetwork/bus/enums/JposEnum.java
@@ -0,0 +1,5 @@
+package net.scnetwork.bus.enums;
+
+public enum JposEnum {
+    SERVER,CLIENT,BOTH,SERVER_PROXY,CLIENT_PROXY,BOTH_PROXY;
+}
