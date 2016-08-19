25e1c211df676beabc578f71eb92bf8318f0a7a0
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-08-19 23:18:55 +0300

    [ALL]: документирование.

--- src/main/java/net/scnetwork/bus/utils/JsonUtils.java
+++ src/main/java/net/scnetwork/bus/utils/JsonUtils.java
@@ -4,7 +4,17 @@ import net.scnetwork.bus.domain.DataJs;
 import net.scnetwork.bus.domain.ResponseJs;
 import net.scnetwork.bus.enums.StatusEnum;
 
+/**
+ * Формирование JSON сообщений
+ */
 public class JsonUtils {
+    private JsonUtils(){}
+
+    /**
+     * Формирование сообщения об ошибке в формате JSON
+     * @param status тип ошибки
+     * @return JSON сообщение
+     */
     public static ResponseJs getError(StatusEnum status){
         ResponseJs result = new ResponseJs();
         DataJs data = new DataJs();
