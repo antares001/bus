8727b7344c9cb8eb5ae2e18f06ed0b8e234590a6
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-08-21 14:33:03 +0300

    [UI]: delete old implementation.

--- src/main/java/net/scnetwork/bus/ui/util/ErrorsUI.java
+++
@@ -1,9 +0,0 @@
-package net.scnetwork.bus.ui.util;
-
-/**
- * Ошибки UI
- */
-public class ErrorsUI {
-    public static final String ERROR_MESSAGE = "Администрирование на этом узле отключено";
-    public static final String FATAL_ERROR = "Непредвиденная ошибка";
-}
