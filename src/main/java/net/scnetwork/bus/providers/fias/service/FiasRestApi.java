f17b093bb6a556586a41592216d3737eeea4d4a7
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-10-09 00:08:09 +0300

    [FIAS]: get url rest

--- src/main/java/net/scnetwork/bus/providers/fias/service/FiasRestApi.java
+++ src/main/java/net/scnetwork/bus/providers/fias/service/FiasRestApi.java
@@ -1,18 +1,72 @@
 package net.scnetwork.bus.providers.fias.service;
 
+import net.scnetwork.bus.clients.fias.DownloadFileInfo;
+import net.scnetwork.bus.clients.fias.DownloadService;
+import net.scnetwork.bus.clients.fias.DownloadServiceSoap;
 import net.scnetwork.bus.config.Config;
 import net.scnetwork.bus.config.Modules;
 import net.scnetwork.bus.providers.fias.config.Fias;
+import net.scnetwork.bus.providers.fias.enums.FiasFormatEnum;
+import net.scnetwork.bus.providers.fias.enums.FiasOperation;
 import net.scnetwork.bus.rest.RestApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotNull;
+import javax.ws.rs.QueryParam;
 
 /**
  * Настройка модуля ФИАС
  */
+@RestController
 public class FiasRestApi implements RestApi{
+    @Autowired
+    private Fias fias;
+
+    /**
+     * Операции fias
+     * @param type тип операции
+     * @return данные
+     */
+    @RequestMapping(value = "/rest/modules/operation/{type}")
+    public String operation(@PathVariable(value = "type") String type, @QueryParam(value = "format") String format){
+        DownloadService service = new DownloadService();
+        DownloadServiceSoap serviceSoap = service.getDownloadServiceSoap();
+        switch (FiasOperation.valueOf(type)){
+            case GET_URL:
+                DownloadFileInfo fileInfo = serviceSoap.getLastDownloadFileInfo();
+                switch (FiasFormatEnum.valueOf(format)){
+                    case ARJ:
+                        return fileInfo.getKladr4ArjUrl();
+                    case DBF:
+                        return fileInfo.getFiasDeltaDbfUrl();
+                    case SEVEN_Z:
+                        return fileInfo.getKladr47ZUrl();
+                    case TEXT:
+                        return fileInfo.getTextVersion();
+                    case XML:
+                        return fileInfo.getFiasDeltaXmlUrl();
+                    default:
+                        break;
+                }
+                break;
+            case GET_ALL_URLS:
+                break;
+            case SYNC:
+                break;
+            case MONGODB:
+                break;
+            case REDIS:
+                break;
+            default:
+                break;
+        }
+        return type;
+    }
+
     @RequestMapping(value = "/rest/api/modules/fias/get/info")
     @Override
     public String getConfig() {
