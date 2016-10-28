228b1a2518d864e2aaee9512e499443e0192bfa7
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-10-28 08:41:38 +0300

    [all]: reconfig

--- src/main/java/net/scnetwork/bus/utils/LogBus.java
+++ src/main/java/net/scnetwork/bus/utils/LogBus.java
@@ -1,6 +1,7 @@
 package net.scnetwork.bus.utils;
 
 import net.scnetwork.bus.config.Config;
+import net.scnetwork.bus.config.Global;
 import net.scnetwork.bus.config.LogConfig;
 import net.scnetwork.bus.enums.LogEnums;
 import org.slf4j.Logger;
@@ -16,8 +17,13 @@ public class LogBus {
 
     private LogBus(){
         try{
-            logConfig = Config.getInstance().getLogConfig();
-        } catch (NullPointerException e){
+            Global instance = Config.getInstance();
+            if (null != instance) {
+                logConfig = instance.getLogConfig();
+            } else {
+                throw new Exception("Error in config file");
+            }
+        } catch (Exception e){
             writeLog(e);
         }
     }
@@ -27,6 +33,28 @@ public class LogBus {
      * @param message сообщение
      */
     public static void writeLog(Exception message){
+        switch (LogEnums.valueOf(logConfig.getType())){
+            case LOCAL:
+                logger.info(message.toString());
+                break;
+            case POSTGRESQL:
+            case MYSQL:
+            case ORACLE:
+            case SQLITE:
+            case SYSLOG:
+            case LOGSTASH:
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Запись инцедентов с указанием системы
+     * @param message сообщение
+     * @param system система
+     */
+    public static void writeSystemLog(Exception message, String system){
+        Logger logger = LoggerFactory.getLogger(system);
         switch (LogEnums.valueOf(logConfig.getType())){
             case LOCAL:
                 logger.info(message.toString());
