4f63c25c2d6514ffe380a90ff4a9321cf99722bf
bloshkin.nikolai <bloshkin.nikolai@gmail.com>
2016-11-03 08:56:19 +0300

    [redmine]: getProjects.

--- src/main/java/net/scnetwork/bus/clients/tracking/redmine/RedmineImpl.java
+++ src/main/java/net/scnetwork/bus/clients/tracking/redmine/RedmineImpl.java
@@ -1,12 +1,25 @@
 package net.scnetwork.bus.clients.tracking.redmine;
 
+import com.taskadapter.redmineapi.ProjectManager;
+import com.taskadapter.redmineapi.RedmineException;
+import com.taskadapter.redmineapi.RedmineManager;
+import com.taskadapter.redmineapi.RedmineManagerFactory;
+import com.taskadapter.redmineapi.bean.Project;
 import net.scnetwork.bus.clients.tracking.ITracking;
 import net.scnetwork.bus.utils.LogBus;
+
+import java.util.List;
 
 /**
  * Реализация клиента для Redmine
  */
 public class RedmineImpl implements ITracking{
+    private RedmineManager manager;
+
+    public RedmineImpl(RedmineConfig config){
+        manager = RedmineManagerFactory.createWithUserAuth(config.getUri(), config.getUsername(), config.getPassword());
+    }
+
     @Override
     public void getInfo() {
         LogBus.info("otrs");
@@ -15,5 +28,13 @@ public class RedmineImpl implements ITracking{
     @Override
     public void setInfo() {
         LogBus.info("otrs");
+    }
+
+    public List<Project> getProgects() throws RedmineException {
+        ProjectManager projectManager = manager.getProjectManager();
+        if (null != projectManager) {
+            return projectManager.getProjects();
+        }
+        return null;
     }
 }
